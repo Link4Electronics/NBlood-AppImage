3162b619509e4e7ab2f4aed8d5ae3ec1118e42e4
Link4Electronics <link4electronics@gmail.com>
2026-02-03 07:51:59 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,13 +3,12 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q nblood-git | awk '{print $2; exit}') # example command to get version of application here
+#VERSION=$(pacman -Q nblood-git | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/pixmaps/nblood.ico
-export DESKTOP=/usr/share/applications/nblood.desktop
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
