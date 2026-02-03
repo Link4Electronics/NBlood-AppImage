886eb4ee7aec6698810f3bc9bf75765ae99be3b1
Link4Electronics <link4electronics@gmail.com>
2026-02-03 07:51:13 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,14 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm
+pacman -Syu --noconfirm \
+    flac     \
+    glu      \
+    libdecor \
+    libogg   \
+    libvorbis \
+    libvpx   \
+    sdl2
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
@@ -14,6 +21,19 @@ get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
 make-aur-package gtk2
-make-aur-package
 
 # If the application needs to be manually built that has to be done down here
+echo "Making nightly build of NBlood..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/NBlood/NBlood"
+VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+git clone --recursive --depth 1 "$REPO" ./NBlood
+echo "$VERSION" > ~/version
+
+cd ./NBlood
+make blood -j$(nproc)
+install -D -t /usr/bin nblood
+install -D -t /usr/share/games/nblood -m 644 nblood.pk3
+install -D -t /usr/share/licenses/nblood -m 644 package/common/buildlic.txt
+install -Dm644 source/blood/rsrc/game_icon.ico /usr/share/pixmaps/nblood.ico
+#install -D -t "$pkgdir"/usr/share/applications -m 644 "$srcdir"/nblood.desktop
